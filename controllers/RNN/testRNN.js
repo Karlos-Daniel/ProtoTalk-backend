b5bff0310d8f8233ac18ff260166749a8332b684
Karlos-Daniel <karlosrodriguez28@gmail.com>
2023-06-23 11:48:31 -0500

    ready to test sockets
    ready to test sockets

--- controllers/RNN/testRNN.js
+++
@@ -1,9 +0,0 @@
-const net = require('./config');
-
-let output = net.run([0, 0]); // [0]
-output = net.run([0, 1]); // [1]
-console.log(output);
-output = net.run([1, 0]); // [1]
-console.log(output);
-output = net.run([1, 1]); // [0]
-console.log(output);
--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,7 +1,18 @@
 
 
 const socketController = (socket)=>{
+        console.log("Un cliente se ha conectado");
 
+        socket.on("disconnect", () => {
+          console.log("Un cliente se ha desconectado");
+        });
+        
+        socket.on("TextOutput", async (output) => {
+          var textoProcesado = await processedText(output);
+          console.log(textoProcesado);
+          socket.emit("ProcessedText", textoProcesado);
+        });
+      
 }
 
 module.exports = {
