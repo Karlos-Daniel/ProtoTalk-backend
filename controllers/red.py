d6456b66b2126ce20f7e261c537999a836cb7119
Karlos-Daniel <68357442+Karlos-Daniel@users.noreply.github.com>
2023-07-05 13:48:14 -0500

    last changes

--- controllers/red.py
+++ controllers/red.py
@@ -50,7 +50,7 @@ def preprocess_text(text):
     # Se definen los STOP WORDS excluyendo los prononmbres y terminos a utilizar 
     stop_words = set(['además', 'ante', 'bajo', 'cabe', 'con', 'contra', 'cuando', 'de', 'desde', 'donde', 'durante',
                       'e', 'a', 'el', 'en', 'entre', 'hacia', 'hasta', 'la', 'al', 'las', 'lo', 'los', 'mis', 'para',
-                      'por', 'qué', 'según', 'sin', 'sobre', 'tras', 'un', 'una','.', ','])
+                      'por', 'qué', 'según', 'sin', 'sobre', 'tras', 'un', 'una','le','del','es','.', ','])
 
     # POS tags permitidos (Pronombres, Verbos, Adjetivos)
     allowed_tags = ['PRON', 'VERB', 'ADJ']
@@ -65,8 +65,16 @@ def preprocess_text(text):
         # Verificar si el token no es una STOP WORD (excluyendo los pronombres) y si el POS tag se encuentra en los tags permitidos
         if token.text.lower() not in stop_words or token.pos_ in allowed_tags:
             # Lemmatize the token
-            lemma = token.lemma_
-            processed_text.append(lemma)
+            """ lemma = token.lemma_
+            processed_text.append(lemma) """
+            if token.ent_type_ == 'PER':
+                # Spell out the name character by character and add it to the processed text
+                spelled_name = list(token.text)
+                processed_text.extend(spelled_name)
+            else:
+                # Lemmatize the token and add it to the processed text
+                lemma = token.lemma_
+                processed_text.append(lemma)
 
     return processed_text
 
--- controllers/socketController.js
+++ controllers/socketController.js
@@ -20,17 +20,22 @@ function processedText (output) {
     });
   }
 
+let GLOSA = []
+
 const socketController = (socket)=>{
         console.log("Un cliente se ha conectado");
 
         socket.on("disconnect", () => {
+          GLOSA = []
           console.log("Un cliente se ha desconectado");
         });
 
         socket.on("TextOutput", async (output) => {
           var textoProcesado = await processedText(output);
+          GLOSA.push(textoProcesado)
           console.log(textoProcesado);
-          socket.emit("ProcessedText", textoProcesado);
+          /* socket.emit("ProcessedText", textoProcesado); */
+          socket.emit("ProcessedText",GLOSA)
         });
       
 }
